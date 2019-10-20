894decfe2e0004e622df74ddffa1fb417b86e9e9
Henrique Marcelino Possatto <henriquempossatto@gmail.com>
2019-10-19 21:30:49 -0300

    removed unecessary files

---
+++ Space_apps/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+    public GameObject menu;
+    private Animation anim;
+    bool act = false;
+
+    void OnEnable()
+    {
+   
+        anim = menu.GetComponent<Animation>();
+    }
+
+    void TongueOut()
+    {
+        anim.Play("TongueOut");
+    }
+    void TongueIn()
+    {
+        anim.Play("TongueIn");
+    }
+
+    public void onClick()
+    {  
+        if (act)
+        {
+            TongueOut();
+        }
+        else
+        {
+            TongueIn();
+        }
+        act = !act;
+    }
+}
